41126d8f5f00f0523d1fbc52fe73a42e9e63c66c
cassie_c <cassie.nightingale.13.13@gmail.com>
2023-06-02 17:55:18 -0500

    add Api

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 <!-- Please update value in the {}  -->
 
-<h1 align="center">{Your project name}</h1>
+<h1 align="center">Random Quote Generator</h1>
 
 <div align="center">
    Solution for a challenge from  <a href="http://devchallenges.io" target="_blank">Devchallenges.io</a>.
@@ -26,16 +26,17 @@
 
 ## Table of Contents
 
-- [Overview](#overview)
-  - [Built With](#built-with)
+<!-- - [Overview](#overview) -->
+
+- [Built With](#built-with)
 - [Features](#features)
-- [How to use](#how-to-use)
+<!-- - [How to use](#how-to-use) -->
 - [Contact](#contact)
 - [Acknowledgements](#acknowledgements)
 
 <!-- OVERVIEW -->
 
-## Overview
+<!-- ## Overview
 
 ![screenshot](https://user-images.githubusercontent.com/16707738/92399059-5716eb00-f132-11ea-8b14-bcacdc8ec97b.png)
 
@@ -44,15 +45,17 @@ Introduce your projects by taking a screenshot or a gif. Try to tell visitors a 
 - Where can I see your demo?
 - What was your experience?
 - What have you learned/improved?
-- Your wisdom? :)
+- Your wisdom? :) -->
 
 ### Built With
 
 <!-- This section should list any major frameworks that you built your project using. Here are a few examples.-->
 
+- HTML
+- CSS
+- JavaScript
+- [Chrome DevTools](https://developer.chrome.com/docs/devtools/)
 - [React](https://reactjs.org/)
-- [Vue.js](https://vuejs.org/)
-- [Tailwind](https://tailwindcss.com/)
 
 ## Features
 
@@ -60,12 +63,11 @@ Introduce your projects by taking a screenshot or a gif. Try to tell visitors a 
 
 This application/site was created as a submission to a [DevChallenges](https://devchallenges.io/challenges) challenge. The [challenge](https://devchallenges.io/challenges/8Y3J4ucAMQpSnYTwwWW8) was to build an application to complete the given user stories.
 
-
-## How To Use
+<!-- ## How To Use -->
 
 <!-- For example: -->
 
-To clone and run this application, you'll need [Git](https://git-scm.com) and [Node.js](https://nodejs.org/en/download/) (which comes with [npm](http://npmjs.com)) installed on your computer. From your command line:
+<!-- To clone and run this application, you'll need [Git](https://git-scm.com) and [Node.js](https://nodejs.org/en/download/) (which comes with [npm](http://npmjs.com)) installed on your computer. From your command line:
 
 ```bash
 # Clone this repository
@@ -76,7 +78,7 @@ $ npm install
 
 # Run the app
 $ npm start
-```
+``` -->
 
 ## Acknowledgements
 
@@ -88,6 +90,4 @@ $ npm start
 
 ## Contact
 
-- Website [your-website.com](https://{your-web-site-link})
-- GitHub [@your-username](https://{github.com/your-usermame})
-- Twitter [@your-twitter](https://{twitter.com/your-username})
+- [GitHub](https://github.com/yunyun-c)
--- src/App.js
+++ src/App.js
@@ -1,29 +1,111 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+  const [bio, setBio] = useState("");
+  const [quotesByAuthor, setQuotesByAuthor] = useState([]);
+
+  const fetchRandomQuote = async () => {
+    try {
+      const response = await fetch("https://api.quotable.io/random");
+      const data = await response.json();
+      const { content, author: quoteAuthor } = data;
+      setQuote(content);
+      setAuthor(quoteAuthor);
+      setQuotesByAuthor([]);
+      getAuthorBio(quoteAuthor);
+    } catch (error) {
+      console.log("Error fetching quote:", error);
+    }
+  };
+
+  const getAuthorBio = async (slug) => {
+    try {
+      const queryParams = new URLSearchParams({
+        slug: slug,
+        limit: "1",
+      });
+      const url = `https://api.quotable.io/authors?${queryParams.toString()}`;
+      const response = await fetch(url);
+
+      if (response.ok) {
+        const data = await response.json();
+        if (data.count > 0) {
+          const author = data.results[0];
+          const bio = author.bio;
+          setBio(bio);
+        } else {
+          setBio("Author not found.");
+        }
+      } else {
+        throw new Error("Request failed with status: " + response.status);
+      }
+    } catch (error) {
+      console.error("Error retrieving author bio:", error.message);
+      setBio("Error retrieving author bio.");
+    }
+  };
+
+  const fetchQuotesByAuthor = async () => {
+    try {
+      const response = await fetch(
+        `https://api.quotable.io/quotes?author=${author}`
+      );
+      const data = await response.json();
+      const quotes = data.results.map((quote) => quote.content);
+      setQuotesByAuthor(quotes);
+    } catch (error) {
+      console.log("Error fetching quotes by author:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchRandomQuote();
+  }, []);
+
+  const handleRandomQuote = () => {
+    fetchRandomQuote();
+  };
+
+  const handleAuthorQuotes = () => {
+    fetchQuotesByAuthor();
+  };
+
   return (
     <div>
       <header className="App-header">
         <h1>Random Quote Generator</h1>
-        <button className="header-btn">
-          Random <span class="material-icons">autorenew</span>
+        <button className="header-btn" onClick={handleRandomQuote}>
+          Random <span className="material-icons">autorenew</span>
         </button>
       </header>
       <main>
         <div className="quotes">
-          <p className="single-quote">
-            "You're braver than you believe, and stronger than you seem, and
-            smarter than you think. "
-          </p>
-          <div className="author-btn">
+          <p className="single-quote">{quote}</p>
+          <div className="author-btn" onClick={handleAuthorQuotes}>
             <div id="author">
-              Author <br />
-              <small>leads</small>
+              {author} <br />
             </div>
-
-            <span class="material-icons btn">arrow_right_alt</span>
+            <span className="material-icons btn">arrow_right_alt</span>
           </div>
         </div>
+        {quotesByAuthor.length > 0 && (
+          <div className="author-quotes">
+            <h2>More quotes by {author}</h2>
+            <span>{bio}</span>
+            <div className="other-quotes">
+              {quotesByAuthor.map((quote, index) => (
+                <div className="quotes">
+                  <p className="single-quote" key={index}>
+                    {quote}
+                  </p>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
       </main>
       <footer>
         <p>C</p>
